9bd7c27f043d5c0c3ccf0fb4fc9e3df13cb6c71d
Ken Chatfield <ken@robots.ox.ac.uk>
2015-04-03 17:51:03 +0100

    Small fix to compILSVRCFeats

--- matlab/caffe/vis/compILSVRCFeats.m
+++ matlab/caffe/vis/compILSVRCFeats.m
@@ -1,4 +1,4 @@
-function compILSVRCFeats(set_name)
+function codes = compILSVRCFeats(set_name)
 
 assert(nargin >= 1);
 
@@ -26,18 +26,38 @@ idxs = (imdb.images.set == set_idx);
 ids = imdb.images.id(idxs);
 names = imdb.images.name(idxs);
 
-codes = ones(encoder.get_output_dim(), length(names), 'single');
-for i = 1:length(names)
-    fprintf('Computing feature %d/%d: %s...\n', i, length(names), names{i});
+output_dims = encoder.get_output_dim();
+output_blob_names = encoder.output_blob_names;
 
-    im = imread(fullfile(imdb.imageDir, names{i}));
+if length(output_blob_names) > 1
+    assert(iscell(output_dims));
+
+    codes = struct();
+    for blb_idx = 1:length(output_dims)
+        codes.(output_blob_names{blb_idx}) = ones(output_dims{blb_idx}, length(names), 'single');
+    end
+else
+    codes = ones(output_dims, length(names), 'single');
+end
+
+for im_idx = 1:length(names)
+    fprintf('Computing feature %d/%d: %s...\n', im_idx, length(names), names{im_idx});
+
+    im = imread(fullfile(imdb.imageDir, names{im_idx}));
     im = featpipem.utility.standardizeImage(im);
 
     a_tic = tic;
-    codes(:, im_idx) = encoder.encode(im);
+    code = encoder.encode(im);
+    if length(output_blob_names) > 1
+        for blb_idx = 1:length(code)
+            codes.(output_blob_names{blb_idx})(:, im_idx) = code{blb_idx};
+        end
+    else
+        codes(:, im_idx) = code;
+    end
     fprintf('Computed in %d seconds\n', toc(a_tic));
 end
 
-save(sprintf('ILSVRC_feats_%s.mat', lower(set_name)), 'codes');
+save(sprintf('ILSVRC_feats_%s.mat', lower(set_name)), 'codes', '-v7.3');
 
 end
